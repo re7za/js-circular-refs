57771dbed7613f297eb2110dd969b533b9169afd
smrsan <smrsan@users.noreply.github.com>
2021-03-06 16:26:40 +0330

    Add modifyObj arg to decycle()

--- src/utils/decycle.ts
+++ src/utils/decycle.ts
@@ -4,17 +4,17 @@ import { TAnyObj } from "../types";
 import randomStr from "./randomStr";
 import isPureObj from "./isPureObj";
 
-export default function decycle(circularObj: TAnyObj): TAnyObj {
+export default function decycle(circularObj: TAnyObj, modifyObj: boolean = false): TAnyObj {
   const refs = new Map<TAnyObj, string>();
 
-  const result: TAnyObj = (function objTreeIterator(obj: TAnyObj) {
-    const result: TAnyObj = {};
+  const result: TAnyObj = (function objTreeIterator(obj: TAnyObj): TAnyObj {
+    const result: TAnyObj = modifyObj ? obj : { ...obj };
     const objId = randomStr(REF_ID_LENGTH);
 
     refs.set(obj, objId);
     result[ID_PROP_NAME] = objId;
 
-    for (const [key, subObj] of Object.entries(obj)) {
+    for (const [key, subObj] of Object.entries(result)) {
       if (!isPureObj(subObj)) continue;
       if (refs.has(subObj)) {
         const subObjId = refs.get(subObj);
