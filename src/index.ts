444d72b40e594de7842e79f480f19a4479d36c5d
smrsan <smrsan@users.noreply.github.com>
2021-03-06 16:27:23 +0330

    Add some misc tests to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import { TAnyObj } from "./types";
 import decycle from "./utils/decycle";
+import retrieve from "./utils/retrieve";
 
-const a: TAnyObj = {};
-const b: TAnyObj = {};
+const a: TAnyObj = { name: "hello" };
+const b: TAnyObj = { fami: "world" };
 
 a.b = b;
 b.a = a;
+b.b = b;
+a.a = a;
 
-console.log(decycle(a));
+const original = a;
+console.log("A (original):", a);
+
+const decycled = decycle(a, true);
+console.log("A (decycled):", a);
+
+const retrieved = retrieve(a, true);
+console.log("A (retrived):", a);
+
+console.log("original === decycled ===", original === decycled);
+console.log("decycled === retrieved ===", decycled === retrieved);
